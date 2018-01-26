9ebebeb251d7c3d2a5836fa162811c4e626b8d4a
Anthony Wong <awong6201@gmail.com>
2018-01-25 17:46:50 -0800

    Added database support for the gallery images

---
+++ application/models/Images.php
@@ -0,0 +1,24 @@
+<?php
+
+class Images extends CI_Model {
+    
+    //Constructor
+    function __construct() {
+        parent::__construct();
+    }
+    
+    //return all images, descending order by post date
+    function all() {
+        $this->db->order_by("id", "desc");
+        $query = $this->db->Get('images');
+        return $query->result_array();
+    }
+    
+    //return just the 3 newest images
+    function newest() {
+        $this->db->order_by("id", "desc");
+        $this->db->limit(3);
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+}
